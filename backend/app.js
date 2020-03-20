4736aa2cc53cd91c6307ebb0f36d907905a1e026
malva <you@example.com>
2020-03-20 17:58:47 +0000

    version where add prod form is not working

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const passport      = require('passport');
 require('./configs/passport');
 
 mongoose
-  .connect('mongodb://localhost/backend', {
+  .connect(process.env.MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
